24c9718ae37660188f19ce33011e2a8763668581
prabh-op <prabhjit.singh@tickmark.io>
2025-04-01 11:34:55 +0530

    feat: update swagger configuration for improved API documentation; add nodemon configuration for automatic file watching

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -1,20 +1,23 @@
 import swaggerJSDoc from 'swagger-jsdoc';
 
 const definition = {
+definition:{
 openapi: "3.0.0",
 info: {
 title: "Dummy",
 version: "1.0.0",
 description: "My API Description",
 },
+},
+apis: ['swagger.yaml'],
 };
 
-const options = {
-swaggerDefinition :definition,
-apis: ['swagger.yaml'], // Path to the API docs
-};
+// const options = {
+//  // Path to the API docs
+// swaggerDefinition :definition,
+// };
 
-const swaggerSpec = swaggerJSDoc(options);
+const swaggerSpec = swaggerJSDoc(definition);
 
 
 export default swaggerSpec;
